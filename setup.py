4e5a50ca29e68de679dec011b878b27482886458
Mayra Azevedo <mayradazevedo@gmail.com>
2019-03-18 10:33:54 -0300

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='":chart_with_upwards_trend: :bar_chart: Analysis on budgets for schools for ML class' project. Originally, a competition by DrivenData https://www.drivendata.org/competitions/46/box-plots-for-education-reboot/',
+    author='nymarya',
+    license='MIT',
+)
